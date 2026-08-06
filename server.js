c4372c938e3257267366deba82b0cfcfdaeb550e
emilymelo140-jpg <emilymelo140@gmail.com>
2026-08-06 12:10:25 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/alunos',(reg,res)=>{
     res.send("lendo dados do banco")
 })
 
+app.post("/aluno", (req,res) => {
+   res.send(`Nome: ${req.body.Nome} Idade: ${req.body.Idade}`)
+
+})
 app.listen(3000, () => {
-    console.log('servidor rodando na porta 3000');
-});
+  console.log("serviço rodando na porta 3000")
+})
+
