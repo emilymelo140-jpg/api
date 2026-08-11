493ae8ff671c15e72f5cc5b2c40c43be9a7f555c
emilymelo140-jpg <emilymelo140@gmail.com>
2026-08-11 11:51:08 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -15,3 +15,12 @@ app.listen(3000, () => {
   console.log("serviço rodando na porta 3000")
 })
 
+const alunos = [
+{"nome": "Emilao","idade", "genero", "CPF"}
+{"nome": "Misão","idade", "genero", "CPF" }
+{"nome": "Lyrao","idade", "genero" , "CPF"}
+{"nome": "Juliao","idade", "genero", "CPF"}
+{"nome": "Itallozao","idade","genero", "CPF"}
+]
+
+
